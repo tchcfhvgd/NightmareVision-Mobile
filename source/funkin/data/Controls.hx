0832566d3f0ee4cf5605cab344750cd2ebeed405
tchcfhvgd <2220687506@qq.com>
2025-07-02 19:28:05 +0800

    fixes

--- source/funkin/data/Controls.hx
+++ source/funkin/data/Controls.hx
@@ -384,7 +384,7 @@ class Controls extends FlxActionSet
 	static function init():Void
 	{
 		var actions = new FlxActionManager();
-		FlxG.inputs.add(actions);
+		FlxG.inputs.addUniqueType(actions);
 	}
 
 	/**
