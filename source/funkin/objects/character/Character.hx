8813821b07bbc429be4dc1a82ce0ce1822495758
tchcfhvgd <2220687506@qq.com>
2025-07-03 10:35:20 +0800

    舒服了

--- source/funkin/objects/character/Character.hx
+++ source/funkin/objects/character/Character.hx
@@ -101,7 +101,7 @@ class Character extends FlxSprite
 	/**
 	 *	if enabled, ghosts will show on double notes for the character
 	 */
-	public var ghostsEnabled:Bool = false;
+	public var ghostsEnabled:Bool = true;
 	
 	/**
 	 * Array of all ghosts
